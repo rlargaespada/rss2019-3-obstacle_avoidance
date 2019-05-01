b70e2e7da0b452e46b177ac761ab4b7b883af801
Kayla <kholman@mit.edu>
2019-05-01 17:15:58 -0400

    added interface to build graph from occupancy grid

--- src/graph_builder.py
+++ src/graph_builder.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import graph
 import search
 
-class AStar:
+class Graph_Build:
     """
     A* path planning from ROS map
     """
@@ -29,9 +29,7 @@ class AStar:
         self.goal_pose = [0, 0, 0] # x, y, theta
         self.goal_size = rospy.get_param("~goal_size")
         self.map_topic = rospy.get_param("~map_topic")
-        self.goal_region = {"xmin": 0, "xmax": 0, "ymin": 0, "ymax": 0} # setup in set_goal
         self.map_res = rospy.get_param("~map_res")
-        self.max_angle = rospy.get_param("~max_angle")
         self.PARTICLE_CLOUD_TOPIC = rospy.get_param("~particle_cloud")
         self.PATH_TOPIC = rospy.get_param("~path_topic")
         self.base_link = rospy.get_param("~base_link")
@@ -40,7 +38,6 @@ class AStar:
         self.origin_y_offset = rospy.get_param("~origin_y_offset")
         self.map_from_file = rospy.get_param("~map_from_file")
         self.path_from_file = rospy.get_param("~path_from_file")
-        self.path_step = 0.05
 
         self.goal_list = []
 
@@ -100,28 +97,13 @@ class AStar:
 
         self.goal_pose = goal
         self.goal_list.append(goal)
-        r = self.goal_size/2
-
-        self.goal_region["xmin"] = x-r
-        self.goal_region["xmax"] = x+r
-        self.goal_region["ymin"] = y-r
-        self.goal_region["ymax"] = y+r
 
     def map_callback(self, map_msg):
-        while self.start_pose == [0, 0, 0]:# or self.goal_pose == [0, 0, 0]:
+        while self.start_pose == [0, 0, 0] or self.goal_pose == [0, 0, 0]:
             continue
 
-        while self.goal_pose == [0,0,0]:
-            if self.path_from_file:
-                break      
-
-
-
-        #self.start_pose = (round(self.start_pose[0]*2, 0)/2., round(self.start_pose[1]*2, 0)/2.)
         self.start_pose = (round(self.start_pose[0], 1), round(self.start_pose[1], 1))
-        #self.goal_pose = (round(self.goal_pose[0]*2, 0)/2., round(self.goal_pose[1]*2, 0)/2.)
         self.goal_pose = (round(self.goal_pose[0], 1), round(self.goal_pose[1], 1))
-        print(self.start_pose, self.goal_pose)
         time1 = rospy.get_time()
 
         print "Loading map:", rospy.get_param("~map"), "..."
@@ -139,9 +121,6 @@ class AStar:
             for j in range(self.map_copy.shape[1]):
                 if self.map_copy[i, j] != 0:
                     self.map[i-self.buff_factor: i+self.buff_factor, j-self.buff_factor: j+self.buff_factor] = 1.0
-        # Convert the origin to a tuple
-        # plt.imshow(self.map)
-        # plt.show()
         origin_p = map_msg.info.origin.position
         origin_o = map_msg.info.origin.orientation
         origin_o = tf.transformations.euler_from_quaternion((
@@ -150,22 +129,6 @@ class AStar:
                 origin_o.z,
                 origin_o.w))
         self.origin = (origin_p.x+self.origin_x_offset, origin_p.y+self.origin_y_offset, origin_o[2])
-        if self.map_name == "stata_basement":
-            self.full_region = {
-                                "xmin": -map_msg.info.width*self.map_res + float(self.origin[0]),
-                                "xmax": float(self.origin[0]),
-                                "ymin": -map_msg.info.height*self.map_res + float(self.origin[1]),
-                                "ymax": float(self.origin[1])
-                                }
-            self.map_flip_const = -1.
-        if self.map_name == "building_31":
-            self.full_region = {
-                                "xmin": float(self.origin[0]),
-                                "xmax": map_msg.info.width*self.map_res + float(self.origin[0]),
-                                "ymin": float(self.origin[1]),
-                                "ymax": map_msg.info.height*self.map_res + float(self.origin[1])
-                                }
-            self.map_flip_const = 1.
 
         #map is an array of zeros and ones, convert into graph
         self.map_graph = graph.Graph(tuple(self.start_pose[:2]), tuple(self.goal_pose[:2]))
@@ -175,45 +138,24 @@ class AStar:
         self.map_loaded = True
         print("yay, we built the graph!")
 
-        path_fn = './astar_path.txt'
-        if not self.path_from_file:
-            #run astar to create path
-            confirm = raw_input('Have you finished setting goal points? Enter any key to confirm.')
-            time2 = rospy.get_time()
-            while len(self.goal_list)>0:
-                self.goal_pose = self.goal_list.pop(0)
-                self.path = self.path + search.a_star(self.map_graph, tuple(self.start_pose[:2]), tuple(self.goal_pose[:2]))
-                self.start_pose = self.goal_pose
-            
-            print(time2-time1)
-            print(rospy.get_time()-time2)
-            #saving path to txt file
-            with open(path_fn, 'w') as f:
-            	for coord in self.path:
-            		f.write("{}\n".format(coord))
-            	print 'written path to ' + path_fn
-        else:
-            #load path from txt file
-	    	self.path = []
-	    	with open(path_fn, 'r') as f:
-	    		print 'opened path from ' + path_fn
-	    		for coord in f:
-	    			t = tuple(float(c) for c in coord[1:-2].split(','))
-	    			self.path.append(t)
+        # everything past here is A* related
 
-        print("       ")
-        print(len(self.path))
-        cost = 0
-        for i in range(len(self.path)-1):
-            cost += self.map_graph.cost(self.path[i], self.path[i+1])
-        print(cost)
+        # while len(self.goal_list)>0:
+        #     self.goal_pose = self.goal_list.pop(0)
+        #     self.path = self.path + search.a_star(self.map_graph, tuple(self.start_pose[:2]), tuple(self.goal_pose[:2]))
+        #     self.start_pose = self.goal_pose
+        
 
-        self.PointCloud_path(self.path)
-        self.path_publisher.publish(self.cloud)
+        # cost = 0 # to calculate path distance
+        # for i in range(len(self.path)-1):
+        #     cost += self.map_graph.cost(self.path[i], self.path[i+1])
+        # print(cost)
 
-        while True:
-            self.particle_cloud_publisher.publish(self.cloud)
-            # print("there should be a motherfucking particle cloud")
+        # self.PointCloud_path(self.path)
+        # self.path_publisher.publish(self.cloud)
+
+        # while True:
+        #     self.particle_cloud_publisher.publish(self.cloud)
 
     def PointCloud_path(self, points):
         self.cloud.header.frame_id = "/map"
