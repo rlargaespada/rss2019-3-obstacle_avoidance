996ea09a2de9f7b39853005ab09b1182f9235167
rlargaes <rlargaes@mit.edu>
2019-05-03 19:23:35 -0400

    graph handles map subset, cleaned up graph_builder

--- src/graph.py
+++ src/graph.py
@@ -8,12 +8,15 @@ class Graph(object):
 	def __init__(self):
 		self.nodes = set()
 		self.neighbors = {} # dict of node mapped to edges
+		self.x_min = 0
+		self.y_min = 0
 		self.x_max = None
 		self.y_max = None
 		self.resolution = None
 		self.origin = tuple()
-		self.map_resolution = '0.1m'
-		#self.map_resolution = 0.5
+		self.fn_resolution = '0.1m'
+		self.fn = None
+		self.params_fn = None
 
 	def cost(self, p1, p2):
 		return ((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)**(0.5)
@@ -28,10 +31,12 @@ class Graph(object):
 		self.neighbors[node1].add(node2)
 
 	def remove_node(self, node):
-		self.nodes.remove(node)
+		#robust if a node isn't found in the graph
+		self.nodes.discard(node)
 		for n in self.neighbors[node]:
 		    self.neighbors[n].discard(node)
-		del self.neighbors[node]
+		res = self.neighbors.pop(node, None)
+		return res
 
 	def __str__(self):
 		for node in self.neighbors:
@@ -55,26 +60,22 @@ class Graph(object):
 		y = round((-coord[1]*self.resolution + self.origin[1]), 1)
 		return (x,y)
 
-	def build_map(self, map, name, resolution, origin, from_file=False):
-		fn = str(name)+str(self.map_resolution)+'.p'
+	def build_map(self, map, name, resolution, origin, x_bounds=None, y_bounds=None, save_params=False):
+		self.fn = str(name)+ '_' + str(self.fn_resolution)+'.p'
 		self.resolution = resolution
 		self.origin = origin
-		self.x_max = map.shape[0]-1
-		self.y_max = map.shape[1]-1
 
-		if from_file:
-			try: 
-				with open(fn, 'rb') as fp:
-					#print 'loading graph from ' + fn
-					data = pickle.load(fp)
-					self.neighbors = data
-					self.nodes = set(self.neighbors.keys())
-				return
-			except:
-				print('failed to load map from file, generating and saving from scratch')
+		if x_bounds != None:
+			self.x_min = x_bounds[0]
+			self.x_max = x_bounds[1]
+		else: self.x_max = map.shape[0]-1
+		if y_bounds != None:
+			self.y_min = y_bounds[0]
+			self.y_max = y_bounds[1]
+		else: self.y_max = map.shape[1]-1
 
-		for x in range(0, self.x_max+1, 1):
-			for y in range(0, self.y_max+1, 1):
+		for x in range(self.x_min, self.x_max+1, 1):
+			for y in range(self.y_min, self.y_max+1, 1):
 				if map[x,y] == 0:
 					pos = (x,y)
 					rwpose = self.occ_to_real_world(pos)
@@ -82,27 +83,54 @@ class Graph(object):
 					for coord in self.get_neighbor_coords(pos):
 						x_prime = coord[0]
 						y_prime = coord[1]
-						if 0 <= x_prime <= self.x_max and 0 <= y_prime <= self.y_max:
+						if self.x_min <= x_prime <= self.x_max and self.y_min <= y_prime <= self.y_max:
 							if map[x_prime, y_prime] == 0:
 								rwcoord = self.occ_to_real_world(coord)
 								self.add_node(rwcoord)
 								self.add_edge(rwpose, rwcoord)
-		#print 'saving graph to ' + fn
-
-		with open(fn, 'wb') as fp:
+		
+		print('saving graph to ' + self.fn)
+		with open(self.fn, 'wb') as fp:
 			pickle.dump(self.neighbors, fp, protocol=pickle.HIGHEST_PROTOCOL)
-		return
+		if save_params: self.save_params()
+		return self.fn
 
-	def load_map(self, fn):
+	def save_params(self):
+		self.params_fn = self.fn[:-2:] + '_params.p'
+		output = {
+			'fn': self.fn,
+			'resolution': self.resolution,
+			'origin': self.origin,
+			'x_bounds': (self.x_min, self.x_max),
+			'y_bounds': (self.y_min, self.y_max)
+		}
+
+		print('saving params to' + self.params_fn)
+		with open(self.params_fn, 'wb') as fp:
+			pickle.dump(output, fp, protocol=pickle.HIGHEST_PROTOCOL)
+		return self.params_fn
+
+	def load_map(self, fn, params_fn=None):
 		try: 
 			with open(fn, 'rb') as fp:
-				#print 'loading graph from ' + fn
+				print('loading graph from ' + fn)
 				data = pickle.load(fp)
 				self.neighbors = data
 				self.nodes = set(self.neighbors.keys())
-			return
 		except:
 			print('failed to load map from file, make sure file name is correct and file located in ~/.ros')
+
+		if params_fn != None:
+			try:
+				print('loading params from ' + params_fn)
+				data = pickle.load(fp)
+				self.fn = data['fn']
+				self.resolution = data['resolution']
+				self.origin = data['origin']
+				self.x_min, self.x_max = data['x_bounds']
+				self.y_min, self.y_max = data['y_bounds']
+			except:
+				print('failed to load params from file, make sure file name is correct and file located in ~/.ros')
 
 	def heuristic(self, node1, node2):
 		point1 = node1
--- src/graph_builder.py
+++ src/graph_builder.py
@@ -2,67 +2,60 @@
 import rospy
 from nav_msgs.msg import OccupancyGrid
 import numpy as np
-# import dubins
 import tf
 from sensor_msgs.msg import PointCloud
 from geometry_msgs.msg import Point32, PoseWithCovarianceStamped, PoseStamped, Pose, Quaternion, Point
 from nav_msgs.msg import Path
 from std_msgs.msg import Header
-# import scipy.misc
 import matplotlib.pyplot as plt
 import graph
-import search
 
 class Graph_Build:
     """
-    A* path planning from ROS map
+    Builds a graph object from an image map and saves to a pickle file.
     """
     def __init__(self):
         """
         start = [x, y, theta]
         goal = [x, y]
         """
-        # initialize start and goal parameters
+        # topic params
         self.START_TOPIC = rospy.get_param("~start_topic")
         self.GOAL_TOPIC = rospy.get_param("~goal_topic")
+        self.MAP_TOPIC = rospy.get_param("~map_topic")
+        #self.PATH_TOPIC = rospy.get_param("~path_topic")
+
         self.start_pose = [0, 0, 0] # x, y, theta
         self.goal_pose = [0, 0, 0] # x, y, theta
-        self.goal_size = rospy.get_param("~goal_size")
-        self.map_topic = rospy.get_param("~map_topic")
+
+        #map creation parameters
         self.map_res = rospy.get_param("~map_res")
-        self.PARTICLE_CLOUD_TOPIC = rospy.get_param("~particle_cloud")
-        self.PATH_TOPIC = rospy.get_param("~path_topic")
-        self.base_link = rospy.get_param("~base_link")
         self.map_name = rospy.get_param("~map")
+        self.x_bounds = rospy.get_param('x_bounds') #tuple (x_min, y_min) or None
+        self.y_bounds = rospy.get_param('y_bounds') #tuple (x_min, y_min) or None
         self.origin_x_offset = rospy.get_param("~origin_x_offset")
         self.origin_y_offset = rospy.get_param("~origin_y_offset")
-        self.map_from_file = rospy.get_param("~map_from_file")
-        self.path_from_file = rospy.get_param("~path_from_file")
+        self.origin = None
 
         self.goal_list = []
 
         #Initialize visualization variables
         self.buff_factor = rospy.get_param("~buff_map")
-        self.cloud = PointCloud()
-
-        self.raw_path = []
-        self.path = []
-        self.pos_path = []
 
         self.map_loaded = False
         self.map_graph = None
         self.map_file = 'map_file'
 
         # initialize publishers and subscribers
-        self.particle_cloud_publisher = rospy.Publisher(self.PARTICLE_CLOUD_TOPIC, PointCloud, queue_size=10)
-        self.path_publisher = rospy.Publisher(self.PATH_TOPIC, PointCloud, queue_size=10)
+        # self.particle_cloud_publisher = rospy.Publisher(self.PARTICLE_CLOUD_TOPIC, PointCloud, queue_size=10)
+        # self.path_publisher = rospy.Publisher(self.PATH_TOPIC, PointCloud, queue_size=10)
 
 
-        rospy.Subscriber(self.START_TOPIC, PoseWithCovarianceStamped, self.set_start)
-        rospy.Subscriber(self.GOAL_TOPIC, PoseStamped, self.set_goal)
+        # rospy.Subscriber(self.START_TOPIC, PoseWithCovarianceStamped, self.set_start)
+        # rospy.Subscriber(self.GOAL_TOPIC, PoseStamped, self.set_goal)
 
         rospy.Subscriber(
-                self.map_topic,
+                self.MAP_TOPIC,
                 OccupancyGrid,
                 self.map_callback,
                 queue_size=1)
@@ -92,23 +85,19 @@ class Graph_Build:
         Gets goal pose from rviz nav goal marker.
         """
         x, y = goal_pose.pose.position.x, goal_pose.pose.position.y
+        theta = 2*np.arctan(goal_pose.pose.pose.orientation.z/goal_pose.pose.pose.orientation.w)
         #goal = [round(x*2, 0)/2., round(y*2, 0)/2., 0]
-        goal = [round(x, 1), round(y, 1), 0]
+        goal = [round(x, 1), round(y, 1), theta]
 
         self.goal_pose = goal
         self.goal_list.append(goal)
 
     def map_callback(self, map_msg):
-        while self.start_pose == [0, 0, 0] or self.goal_pose == [0, 0, 0]:
-            continue
+        if self.map_loaded: return
 
-        self.start_pose = (round(self.start_pose[0], 1), round(self.start_pose[1], 1))
-        self.goal_pose = (round(self.goal_pose[0], 1), round(self.goal_pose[1], 1))
-        time1 = rospy.get_time()
-
-        print "Loading map:", rospy.get_param("~map"), "..."
-        print "Start and Goal intialized:"
-        print "Start: ", self.start_pose
+        # print "Loading map:", rospy.get_param("~map"), "..."
+        # print "Start and Goal intialized:"
+        # print "Start: ", self.start_pose
         #print "Goal: ", self.goal_pose
         # Convert the map to a numpy array
         map_ = np.array(map_msg.data, np.double)
@@ -117,10 +106,11 @@ class Graph_Build:
         # self.map = np.flip(self.map, axis=0)
         self.map_copy = np.copy(self.map)
         #Beef up the edges
-        for i in range(self.map_copy.shape[0]):
-            for j in range(self.map_copy.shape[1]):
+        for i in range(self.map_copy.shape[0]): #change range to appropriate coordiates of map
+            for j in range(self.map_copy.shape[1]): #change range to appropriate coordiates of map
                 if self.map_copy[i, j] != 0:
                     self.map[i-self.buff_factor: i+self.buff_factor, j-self.buff_factor: j+self.buff_factor] = 1.0
+                    
         origin_p = map_msg.info.origin.position
         origin_o = map_msg.info.origin.orientation
         origin_o = tf.transformations.euler_from_quaternion((
@@ -131,42 +121,16 @@ class Graph_Build:
         self.origin = (origin_p.x+self.origin_x_offset, origin_p.y+self.origin_y_offset, origin_o[2])
 
         #map is an array of zeros and ones, convert into graph
-        self.map_graph = graph.Graph(tuple(self.start_pose[:2]), tuple(self.goal_pose[:2]))
-        # print(map_msg.info.resolution, map_msg.info.origin.position.x, map_msg.info.origin.position.y)
-        self.map_graph.build_map(self.map, self.map_name, map_msg.info.resolution, self.origin, self.map_from_file)
+        self.map_graph = graph.Graph()
+        self.resolution = map_msg.info.resolution
+        print(map_msg.info.resolution, map_msg.info.origin.position.x, map_msg.info.origin.position.y)
+        self.map_graph.build_map(self.map, self.map_name, self.resolution, self.origin, self.x_bounds, self.y_bounds)
 
         self.map_loaded = True
         print("yay, we built the graph!")
 
-        # everything past here is A* related
-
-        # while len(self.goal_list)>0:
-        #     self.goal_pose = self.goal_list.pop(0)
-        #     self.path = self.path + search.a_star(self.map_graph, tuple(self.start_pose[:2]), tuple(self.goal_pose[:2]))
-        #     self.start_pose = self.goal_pose
-        
-
-        # cost = 0 # to calculate path distance
-        # for i in range(len(self.path)-1):
-        #     cost += self.map_graph.cost(self.path[i], self.path[i+1])
-        # print(cost)
-
-        # self.PointCloud_path(self.path)
-        # self.path_publisher.publish(self.cloud)
-
-        # while True:
-        #     self.particle_cloud_publisher.publish(self.cloud)
-
-    def PointCloud_path(self, points):
-        self.cloud.header.frame_id = "/map"
-        self.cloud.header.stamp = rospy.rostime.Time.now()
-        self.cloud.points = [Point32() for i in range(len(points))]
-        for point in range(len(points)):
-            self.cloud.points[point].x = points[point][0]
-            self.cloud.points[point].y = points[point][1]
-            self.cloud.points[point].z = 0
 
 if __name__ == "__main__":
     rospy.init_node("astar")
-    astar = AStar()
+    builder = Graph_Build()
     rospy.spin()
